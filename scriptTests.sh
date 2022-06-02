41d89b50d685852907674312b7236822bc5fcf02
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 16:04:14 -0600

    try fix

--- scriptTests.sh
+++ scriptTests.sh
@@ -14,16 +14,5 @@ testFirstScript() {
     
 }
 
-testSecondScript() {
-    $("${FULLPATH}"/tests/script_3_test -u user1 -p user1)
-    #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
-    EXITCODE=$(echo $?)
-    assertEquals "${EXITCODE}" "0"
-
-    $("${FULLPATH}"/tests/script_3_test -u user1 -d)
-    EXITCODE=$(echo $?)
-    assertEquals "${EXITCODE}" "0"
-    
-}
 
 . shunit2
