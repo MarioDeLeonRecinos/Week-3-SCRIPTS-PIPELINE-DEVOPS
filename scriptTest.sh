f3bce5260391b737b1cf9ae6e1c59eb2a4ea67b4
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 16:22:40 -0600

    try fix

--- scriptTest.sh
+++ scriptTest.sh
@@ -1,8 +1,17 @@
 #! /bin/sh
-# file: secondScriptTest.sh
+# file: scriptTest.sh
 FULLPATH=$(pwd)
 
-testSecondScript() {
+testScripts() {
+    $("${FULLPATH}"/tests/script_2_test -d prueba.txt)
+    #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
+    EXITCODE=$(echo $?)
+    assertEquals "${EXITCODE}" "0"
+
+    $("${FULLPATH}"/tests/script_2_test)
+    EXITCODE=$(echo $?)
+    assertEquals "${EXITCODE}" "0"
+    
     $("${FULLPATH}"/tests/script_3_test -u user1 -p user1)
     #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
     EXITCODE=$(echo $?)
@@ -11,6 +20,6 @@ testSecondScript() {
     $("${FULLPATH}"/tests/script_3_test -u user1 -d)
     EXITCODE=$(echo $?)
     assertEquals "${EXITCODE}" "0"
-    
 }
+
 . shunit2
