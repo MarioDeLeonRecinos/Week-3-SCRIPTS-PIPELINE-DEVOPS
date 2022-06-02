0687d8ef3677efe2b598475759c8b2442ff4994d
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 14:55:13 -0600

    pruebas

--- scriptTests.sh
+++ scriptTests.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 # file: scriptTests.sh
 FULLPATH=$(pwd)
+touch "/tmp/backup_script2/prueba.txt"
 
 #chmod -R 755 "${FULLPATH}/scripts"
 testFirstScript() {
-    touch "/tmp/backup_script2/prueba.txt"
     "${FULLPATH}"/scripts/script_2
     #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
     EXITCODE=$(echo $?)
