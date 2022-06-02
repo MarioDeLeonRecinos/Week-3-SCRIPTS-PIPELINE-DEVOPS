6ef5338455c04b67de07bc29b63f46308d847c6c
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 16:15:40 -0600

    fix test

--- secondScriptTest.sh
+++ secondScriptTest.sh
@@ -2,19 +2,6 @@
 # file: secondScriptTest.sh
 FULLPATH=$(pwd)
 
-testFirstScript() {
-    $("${FULLPATH}"/tests/script_2_test -d prueba.txt)
-    #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
-    EXITCODE=$(echo $?)
-    assertEquals "${EXITCODE}" "0"
-
-    $("${FULLPATH}"/tests/script_2_test)
-    EXITCODE=$(echo $?)
-    assertEquals "${EXITCODE}" "0"
-    
-}
-
-
 testSecondScript() {
     $("${FULLPATH}"/tests/script_3_test -u user1 -p user1)
     #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
