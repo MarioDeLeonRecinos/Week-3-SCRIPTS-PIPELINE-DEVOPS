16b8fe1ebbed062311085d0007700e868a523a72
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 16:17:09 -0600

    try fix

--- firstScriptTest.sh
+++ firstScriptTest.sh
@@ -2,13 +2,13 @@
 # file: firstScriptTest.sh
 FULLPATH=$(pwd)
 
-testSecondScript() {
-    $("${FULLPATH}"/tests/script_3_test -u user1 -p user1)
+testFirstScript() {
+    $("${FULLPATH}"/tests/script_2_test -d prueba.txt)
     #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
     EXITCODE=$(echo $?)
     assertEquals "${EXITCODE}" "0"
 
-    $("${FULLPATH}"/tests/script_3_test -u user1 -d)
+    $("${FULLPATH}"/tests/script_2_test)
     EXITCODE=$(echo $?)
     assertEquals "${EXITCODE}" "0"
     
