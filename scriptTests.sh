b753b6e17d8a18929289bc53b6343b026c5cf47c
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 16:42:57 -0600

    try fix

--- scriptTests.sh
+++ scriptTests.sh
@@ -3,24 +3,31 @@
 FULLPATH=$(pwd)
 
 testScripts() {
-    $("${FULLPATH}"/tests/script_2_test -d prueba.txt)
+    eval "$("${FULLPATH}"/tests/script_2_test -d prueba.txt)"
+    #eval "$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/tests/script_2_test -d /home/mario/github/Week-3-DEVOPS/prueba.txt)"
     #$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2 -d /home/mario/github/Week-3-DEVOPS/prueba.txt)
-    EXITCODE=$(echo $?)
+    EXITCODE=$?
     assertEquals "${EXITCODE}" "0"
 
-    $("${FULLPATH}"/tests/script_2_test)
+    eval "$("${FULLPATH}"/tests/script_2_test)"
+    #eval "$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/tests/script_2_test)"
     #$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2)
-    EXITCODE=$(echo $?)
+    EXITCODE=$?
     assertEquals "${EXITCODE}" "0"
     
-    $("${FULLPATH}"/tests/script_3_test -u user1 -p user1)
+}
+
+testSecondScript(){
+    eval "$("${FULLPATH}"/tests/script_3_test -u user1 -p user1)"
+    #eval "$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/tests/script_3_test -u user1 -p user1)"
     #$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_3 -u user1 -p user1)
-    EXITCODE=$(echo $?)
+    EXITCODE=$?
     assertEquals "${EXITCODE}" "0"
 
-    $("${FULLPATH}"/tests/script_3_test -u user1 -d)
+    eval "$("${FULLPATH}"/tests/script_3_test -u user1 -d)"
+    #eval "$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/tests/script_3_test -u user1 -d)"
     #$("${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_3 -u user1 -d)
-    EXITCODE=$(echo $?)
+    EXITCODE=$?
     assertEquals "${EXITCODE}" "0"
 }
 
