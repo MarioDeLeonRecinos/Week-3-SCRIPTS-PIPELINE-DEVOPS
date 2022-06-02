39b06e938aa0b9e954a688f83bcecaaeac56f1b3
MarioDeLeonRecinos <00353715@uca.edu.sv>
2022-06-02 15:26:10 -0600

    test testes

--- scriptTests.sh
+++ scriptTests.sh
@@ -2,18 +2,28 @@
 # file: scriptTests.sh
 FULLPATH=$(pwd)
 
-#chmod -R 777 "${FULLPATH}/scripts"
 testFirstScript() {
-    $("${FULLPATH}"/scripts/script_2_test -d prueba.txt)
+    $("${FULLPATH}"/tests/script_2_test -d prueba.txt)
     #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
     EXITCODE=$(echo $?)
     assertEquals "${EXITCODE}" "0"
 
-    $("${FULLPATH}"/scripts/script_2_test)
+    $("${FULLPATH}"/tests/script_2_test)
+    EXITCODE=$(echo $?)
+    assertEquals "${EXITCODE}" "0"
+    
+}
+
+testSecondScript() {
+    $("${FULLPATH}"/tests/script_3_test -u user1 user1)
+    #"${FULLPATH}"/Week-3-SCRIPTS-PIPELINE-DEVOPS/scripts/script_2
+    EXITCODE=$(echo $?)
+    assertEquals "${EXITCODE}" "0"
+
+    $("${FULLPATH}"/tests/script_3_test -u user1 -d)
     EXITCODE=$(echo $?)
     assertEquals "${EXITCODE}" "0"
     
 }
 
 . shunit2
-#. ../shunit2
